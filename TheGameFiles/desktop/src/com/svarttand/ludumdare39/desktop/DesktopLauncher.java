eaadb28156f2d18fa670a040b446366c4fc47221
SvartTand <albert24lindberg@gmail.com>
2017-07-28 19:35:20 +0300

    Add files via upload

---
+++ TheGameFiles/desktop/src/com/svarttand/ludumdare39/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.svarttand.ludumdare39.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.svarttand.ludumdare39.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Application(), config);
+	}
+}
