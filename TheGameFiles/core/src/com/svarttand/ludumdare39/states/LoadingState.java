de6515f7c3696901dd9a2286d19fea032305442a
Blackstorm <Blackstorm@86.50.78.77>
2017-07-28 20:16:37 +0300

    I did some basic stuff to make life easier tomorrow

---
+++ TheGameFiles/core/src/com/svarttand/ludumdare39/states/LoadingState.java
@@ -0,0 +1,89 @@
+package com.svarttand.ludumdare39.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.svarttand.ludumdare39.Application;
+
+public class LoadingState extends State {
+		
+		private static final int AUDIO_AMOUNT = 0;
+		private static final String AtlasPath = "uihisud";
+	 	private Viewport viewport;
+	    private boolean loaded;
+	    private int counter;
+	    private String[] audioPaths;
+
+	    public LoadingState(GameStateManager gsm) {
+	        super(gsm);
+	        viewport = new StretchViewport(Application.V_WIDTH, Application.V_HEIGHT, cam);
+	        counter = 0;
+	        loaded = false;
+	        makeAudioPaths();
+	    }
+	    
+	    private void makeAudioPaths(){
+	    	
+	    	audioPaths = new String[AUDIO_AMOUNT];
+	        for (int i = 0; i < AUDIO_AMOUNT; i++) {
+	            audioPaths[i] = i + ".wav";
+	        }
+	      
+	    }
+	    
+
+	    private void load(){
+	        for (int i = 0; i < audioPaths.length; i++) {
+	            gsm.assetManager.load(audioPaths[i], Sound.class);
+	        }
+	        gsm.assetManager.load(AtlasPath, TextureAtlas.class);
+	        loaded = true;
+	    }
+
+	    @Override
+	    protected void handleInput(float delta) {
+
+	    }
+
+	    @Override
+	    public void update(float delta) {
+	        if (!loaded){
+	            load();
+	        }
+	        System.out.println(counter++);
+	        gsm.assetManager.update();
+	        if (gsm.assetManager.getProgress() >= 1){
+	            
+	            gsm.set(new MenuState(gsm));
+	            dispose();
+	            
+	        }
+	    }
+
+	    @Override
+	    public void render(SpriteBatch batch) {
+	        Gdx.gl.glClearColor(1, 0, 0, 1);
+	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	        cam.update();
+	        batch.setProjectionMatrix(cam.combined);
+	        batch.begin();
+	        batch.end();
+
+	    }
+
+	    @Override
+	    public void dispose() {
+
+	    }
+
+	    @Override
+	    public void resize(int width, int height) {
+	        viewport.update(width, height);
+
+	    }
+
+}
